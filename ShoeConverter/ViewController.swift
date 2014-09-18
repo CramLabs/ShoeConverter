0ac3e745d128e78805a7b543a66bd7486215d461
Alex Tuazon <trisoleil@gmail.com>
2014-09-17 18:42:43 -0700

    Showing jacob how to upload files to Github

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -4,13 +4,19 @@
 //
 //  Created by Alex Tuazon on 9/16/14.
 //  Copyright (c) 2014 KTKTC. All rights reserved.
-//
+// 
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var mensShoeSizeTextField: UIView!
+    
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +35,19 @@ class ViewController: UIViewController {
         var integerFromTextField = numberFromTextField!
         let conversionConstant = 30
         integerFromTextField += conversionConstant
+        mensConvertedShoeSizeLabel.hidden = false
+        let stringWithUpdatedShoeSize = "\(integerFromTextField)" + " in European Shoe Size"
+        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+
     }
 
+    @IBAction func convertWomensShoeSizeButtonPressed(sender: AnyObject)
+    {
+        let sizeFromTextField = Double((womensShoeSizeTextField.text as NSString).doubleValue)
+        
+        let conversionConstant = 30.5
+        womensConvertedShoeSizeLabel.hidden = false
+        womensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+    }
 }
 
